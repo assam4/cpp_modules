ed4c78ff05b7a597f353c83bfc4918cf3f2040e6
assam04 <samaslanyan3@gmail.com>
2025-09-29 14:27:23 +0400

    changes cpp08/ex00 Makefile, main.cpp

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -8,23 +8,21 @@ int main() {
         std::cout << "=== Test with std::vector<int> ===" << std::endl;
         std::vector<int> vec;
         for (int i = 0; i < 5; ++i)
-            vec.push_back(i * 10); // 0, 10, 20, 30, 40
-
+            vec.push_back(i * 10);
         std::vector<int>::iterator it1 = easyfind(vec, 20);
         std::cout << "Found in vector: " << *it1 << std::endl;
-
-        // проверим, что можем изменить найденный элемент
         *it1 = 99;
         std::cout << "Modified element: " << vec[2] << std::endl;
-
         std::cout << "\n=== Test with const std::list<int> ===" << std::endl;
-        const std::list<int> lst = {5, 15, 25, 35};
-
+        std::list<int> lst;
+	    lst.push_back(5);
+		lst.push_back(15);
+		lst.push_back(25);
+		lst.push_back(35);
         std::list<int>::const_iterator it2 = easyfind(lst, 25);
         std::cout << "Found in const list: " << *it2 << std::endl;
-
         std::cout << "\n=== Test exception ===" << std::endl;
-        easyfind(vec, 123); // этого элемента нет
+        easyfind(vec, 123);
     }
     catch (const std::exception& e) {
         std::cerr << "Caught exception: " << e.what() << std::endl;
