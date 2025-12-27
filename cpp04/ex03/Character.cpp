bd0c10c153aa4ed3cda8183090ecf02f7b434cd1
Samvel Aslanyan <115891697+assam4@users.noreply.github.com>
2025-12-27 22:22:36 +0400

    Fix equip and unequip methods in Character class
    Clone the item when equipping and delete on unequip.

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -48,14 +48,16 @@ void    Character::equip(AMateria* m) {
 		return;
 	for (int i = 0; i < 4; ++i)
 		if (!m_inventory[i]) {
-			m_inventory[i] = m;
+			m_inventory[i] = m->clone();
 			break ;
 		}
 }
 
 void    Character::unequip(int idx) {
-	if (idx >= 0 && idx < 4)
+	if (idx >= 0 && idx < 4) {
+		delete m_inventory[idx];
 		m_inventory[idx] = NULL;
+	}
 }
 
 void    Character::use(int idx, ICharacter& target) {
