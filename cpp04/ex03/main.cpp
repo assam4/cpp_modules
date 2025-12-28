aa52efeaff071cc8143a28b77fb2edd0cf0d19fd
Samvel Aslanyan <115891697+assam4@users.noreply.github.com>
2025-12-28 20:12:06 +0400

    Update main.cpp

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -1,10 +1,9 @@
-ǰ#include "Ice.hpp"
+#include "Ice.hpp"
 #include "Cure.hpp"
 #include "Character.hpp"
 #include "MateriaSource.hpp"
 
-int main()
-{
+int main() {
 IMateriaSource* src = new MateriaSource();
 src->learnMateria(new Ice());
 src->learnMateria(new Cure());
@@ -12,12 +11,13 @@ ICharacter* me = new Character("me");
 AMateria* tmp;
 tmp = src->createMateria("ice");
 me->equip(tmp);
+delete tmp;
 tmp = src->createMateria("cure");
 me->equip(tmp);
+delete tmp;
 ICharacter* bob = new Character("bob");
 me->use(0, *bob);
 me->use(1, *bob);
-delete tmp;
 delete bob;
 delete me;
 delete src;
