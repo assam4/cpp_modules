f1153644b59e4668db66e8b88b549a3e63c26b28
Samvel Aslanyan <115891697+assam4@users.noreply.github.com>
2025-12-28 20:13:26 +0400

    Fix memory management in Character class

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -1,18 +1,22 @@
 #include "Character.hpp"
 
 Character::Character(): m_name("Undefined") {
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) {
 		m_inventory[i] = NULL;
+		m_unused[i] = NULL;
+	}
 }
 
 Character::Character(const std::string& name): m_name(name) {
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 4; ++i) {
 		m_inventory[i] = NULL;
+		m_unused[i] = NULL;
+	}
 }
 
 Character::~Character() {
 	for (int i = 0; i < 4; ++i)
-		delete m_inventory[i];
+		delete m_unused[i];
 }
 
 Character::Character(const Character& oth) {
@@ -20,8 +24,9 @@ Character::Character(const Character& oth) {
 	for (int i = 0; i < 4; ++i) {
 		if (oth.m_inventory[i])
 			m_inventory[i] = oth.m_inventory[i]->clone();
-		else
+		else 
 			m_inventory[i] = NULL;
+		m_unused[i] = m_inventory[i];
 	}
 }
 
@@ -29,11 +34,12 @@ Character&	Character::operator=(const Character& oth) {
 	if (this != &oth) {
 		m_name = oth.m_name;
 		for (int i = 0; i < 4; ++i) {
-			delete m_inventory[i];
+			delete m_unused[i];
 			if (oth.m_inventory[i])
 				m_inventory[i] = oth.m_inventory[i]->clone();
 			else
 				m_inventory[i] = NULL;
+			m_unused[i] = m_inventory[i];
 		}
 	}
 	return *this;
@@ -48,20 +54,21 @@ void    Character::equip(AMateria* m) {
 		return;
 	for (int i = 0; i < 4; ++i)
 		if (!m_inventory[i]) {
+			delete m_unused[i];
 			m_inventory[i] = m->clone();
+			m_unused[i] = m_inventory[i];
 			break ;
 		}
 }
 
 void    Character::unequip(int idx) {
-	if (idx >= 0 && idx < 4) {
-		delete m_inventory[idx];
+	if (idx >= 0 && idx < 4)
 		m_inventory[idx] = NULL;
-	}
 }
 
 void    Character::use(int idx, ICharacter& target) {
 	if (idx >= 0 && idx < 4 && m_inventory[idx])
 		m_inventory[idx]->use(target);
 }
+
 
